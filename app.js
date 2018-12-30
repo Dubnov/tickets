4855d3db983a8b424d3d65f7c8c93c94c898b00a
Dor Dubnov <ddubnov@gmail.com>
2018-12-30 21:22:10 +0200

    d

--- app.js
+++ app.js
@@ -9,18 +9,29 @@
 
     function controller() {
         const self = this;
+        self.sortOption = 'Recommended'
+        self.sortOptions = ['Recommended', 'Cheapest', 'Best seats', 'Highest price']
+        self.date = new Date();
+        self.areaFilter = {};
 
         self.$onInit = () => {
             self.initRangePrices();
+            self.initAreasFilter();
         }
 
-        self.sortOption = 'Recommended'
-        self.sortOptions = ['Recommended', 'Cheapest', 'Best seats', 'Highest price']
+        self.initAreasFilter = () => {
+            self.areas = [];
+
+            self.tickets.map(x => x.area).forEach(x => {
+                if (!self.areas.some(a => a === x)) {
+                    self.areas.push(x);
+                    self.areaFilter[x] = true;
+                }
+            });
+        }
 
         self.initRangePrices = () => {
             const prices = self.tickets.map(x => x.price);
-            
-
             self.priceSlider = {
                 minValue: Math.min(...prices),
                 maxValue: Math.max(...prices),
@@ -29,6 +40,10 @@
                     ceil: Math.max(...prices),
                 }
             }
+        }
+
+        self.areaFilterFunc = (tick) => {
+            return self.areaFilter[tick.area];
         }
 
         self.sortBy = (option) => {
@@ -73,56 +88,64 @@
                 description: '',
                 price: 300,
                 location: '',
-                amount: 2
+                amount: 2,
+                area: 'Lower Tier'
             },
             {
                 title: 'Section 200',
                 description: '',
                 price: 110,
                 location: '',
-                amount: 2
+                amount: 2,
+                area: 'Middle Tier'
             },
             {
                 title: 'Section 105',
                 description: '',
                 price: 250,
                 location: '',
-                amount: 2
+                amount: 2,
+                area: 'Lower Tier'
             },
             {
                 title: 'Section 210',
                 description: '',
                 price: 100,
                 location: '',
-                amount: 2
+                amount: 2,
+                area: 'Middle Tier'
             },
             {
                 title: 'Section 335',
                 description: '',
                 price: 55,
                 location: '',
-                amount: 2
+                amount: 2,
+                area: 'Middle Tier'
             },
             {
                 title: 'Section 300',
                 description: '',
                 price: 50,
                 location: '',
-                amount: 2
+                amount: 2,
+                area: 'Middle Tier'
             },
             {
                 title: 'Section 400',
                 description: '',
                 price: 10,
                 location: '',
-                amount: 2
+                amount: 2,
+                area: 'Upper Tier'
             },
             {
                 title: 'Section 10',
                 description: '',
                 price: 1000,
                 location: '',
-                amount: 2
+                amount: 2,
+                area: 'Boxes'
             }
         ]
     }
