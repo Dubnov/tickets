3b3d89289f314d5d6c0bd75dad7eccd61a2fe532
Dor Dubnov <ddubnov@gmail.com>
2019-01-04 22:42:04 +0200

    sad

--- app.js
+++ app.js
@@ -5,11 +5,14 @@
     .module("app", ["ngMaterial", "ngMessages", "rzModule"])
     .controller("main", controller);
 
-  controller.$inject = ["$mdDialog"];
+  controller.$inject = ['$mdDialog', '$mdToast'];
 
-  function controller($mdDialog) {
+  function controller($mdDialog, $mdToast) {
     const self = this;
+    self.eventLocation = 'Staples Center, Los Angeles, California, USA';
+    self.ticketsAmount = '';
     self.sortOption = "Recommended";
+    
     self.sortOptions = [
       "Recommended",
       "Cheapest",
@@ -23,11 +26,13 @@
         self.initTickets();
         self.initRangePrices();
         self.initAmountFilter();
-        self.initAreasFilter();        
+        self.initAreasFilter(); 
+        self.sortBy();
     };
 
     self.initAmountFilter = () => {
         self.ticketsAmountOptions = [];
+
         self.tickets.map(x => x.amount).forEach(x => {
             if (!self.ticketsAmountOptions.some(a => a === x)) {
                 self.ticketsAmountOptions.push(x);
@@ -50,6 +55,10 @@
         });
     };
 
+    self.selectAmount = (amount) => {
+        self.ticketsAmount = amount;
+    }
+
     self.initRangePrices = () => {
       const prices = self.tickets.map(x => x.price);
       self.priceSlider = {
@@ -62,6 +71,34 @@
       };
     };
 
+    self.buyTicket = ($event) => {
+        $event.stopPropagation();
+        $mdToast.show(
+            $mdToast.simple()
+            .textContent('Thank you for buying!')
+            .position('')
+            .hideDelay(3000))
+    }
+
+
+    self.toggleFavorite = ($event, ticket) => {
+        $event.stopPropagation();
+        ticket.favorite = !ticket.favorite
+
+        if (ticket.favorite) {
+            $mdToast.show(
+                $mdToast.simple()
+                .textContent('The ticket was successfully added to your favorites!')
+                .position('')
+                .hideDelay(3000))
+        } else {
+            $mdToast.show(
+                $mdToast.simple()
+                .textContent('The ticket was removed from your favorites')
+                .position('')
+                .hideDelay(3000))
+        }
+    }
     self.areaFilterFunc = tick => {
       return self.areaFilter[tick.area];
     };
@@ -94,7 +131,7 @@
           break;
         }
         case "Recommended": {
-          self.tickets = tempTickets.sort((a, b) => b.price - a.price);
+          self.tickets = tempTickets.sort((a, b) => b.score - a.score);
           break;
         }
         default: {
@@ -111,103 +148,98 @@
     }
 
     self.openImage = ($event, imageUrl) => {
-      $mdDialog.show({
-        clickOutsideToClose: true,
-        targetEvent: $event,
-        bindToController: true,
-        controllerAs: 'dialogCtrl',
-        fullscreen: true,
-        template:
-          `<img src="${imageUrl}">`,
-        controller: function DialogController($scope, $mdDialog) {
-          $scope.closeDialog = function() {
-            $mdDialog.hide();
-          };
-        }
-      });
+      
+        $mdDialog.show({
+            clickOutsideToClose: true,
+            targetEvent: $event,
+            bindToController: true,
+            controllerAs: 'dialogCtrl',
+            fullscreen: true,
+            template: `<img src="${imageUrl}">`,
+            controller: function DialogController($scope, $mdDialog) {
+            $scope.closeDialog = function() {
+                $mdDialog.hide();
+            };
+            }
+        });
+        $event.stopPropagation();
     };
 
     self.initTickets = () => {
         self.tickets = window.data;
         self.ticketsDisplayed = angular.copy(self.tickets);
     };
+
+    self.openTicket = (ticket, $event) => {
+        $event.stopPropagation();
+        $mdDialog.show({
+            clickOutsideToClose: true,
+            targetEvent: $event,
+            locals: { ticket, location: self.eventLocation, toggleFavorite: self.toggleFavorite },
+            controllerAs: 'dialogCtrl',
+            fullscreen: true,
+            template: `
+            <md-dialog class="ticket-entity-modal" layout="row" layout-align="space-between">
+                <div class="ticket-main" layout="row" layout-align="start start" layout-margin>
+                    <div layout="column" layout-align="start center">
+                        <img class="ticket-image" src="https://cdn.viagogo.net/img/svgpng4/77551/256963.png" alt=""
+                            ng-click="ctrl.openImage($event, 'https://cdn.viagogo.net/img/svgpng4/77551/256963.png')">
+                        <div layout="column" layout-align="start stretch">
+                            <span>
+                                Area <strong style="color: #287ac7;">{{ticket.area}}</strong>
+                                | Section <strong style="color: #287ac7;">{{ticket.section}}</strong>                                
+                            </span>
+                            <span>
+                                Row <strong style="color: #287ac7;">{{ticket.row}}</strong>
+                                | Seats
+                                <span ng-repeat="s in ticket.seat">
+                                    <strong style="color: #287ac7;">{{s}}</strong><span ng-if="$index !== (ticket.seat.length - 1) ">,</span>
+                                </span>
+                            </span>
+                        </div>
+                    </div>
+                    <div class="ticket-details" flex layout="column" layout-align="start start">
+                        <span class="amount">Number of Tickets: <strong>{{ticket.amount}}</strong></span>
+                        <span class="price">Ticket Price: <strong>{{ticket.price | currency}}</strong></span>
+                        <span class="price">Total: <strong>{{(ticket.price * ticket.amount) | currency}}</strong></span>
+                        <span class="view" ng-class="{'ok': ticket.view.status == '0', 'warning': ticket.view.status == '1', 'error': ticket.view.status == '2'}" layout="row">
+                            {{ticket.view.message}}
+                            <span layout="row" layout-align="center center" style="margin-left: 5px;">
+                                <img src="assets/icons/info.svg" width="15" height="15">
+                                <md-tooltip md-direction="bottom">{{ticket.view.subMsg}}</md-tooltip>
+                            </span>
+                        </span>
+                        <span>
+                            Our Score: <strong>{{ ticket.score }}</strong>
+                        </span>
+                        <span layout="row">
+                            <span>Description:</span>
+                            <span style="margin-left: 5px;">{{ticket.description}}</span>
+                        </span>
+                    </div>
+                </div>
+                <div class="ticket-buy" layout="column" layout-align="end center">
+                    <md-button class="buy-btn md-raised md-primary">Buy</md-button>
+                </div>
+                <md-button ng-if="!ticket.favorite" class="favorite md-icon-button md-accent" aria-label="Favorite" ng-click="toggleFavorite($event, ticket)">
+                    <img  src="assets/icons/favorite-border.svg" alt="favorite">
+                    <md-tooltip md-direction="top">Add to favorites</md-tooltip>
+                </md-button>
+                <md-button ng-if="ticket.favorite" class="favorite md-icon-button md-accent" aria-label="Favorite" ng-click="toggleFavorite($event, ticket)">
+                    <img src="assets/icons/favorite.svg" alt="favorite">
+                    <md-tooltip md-direction="top">Remove from favorites</md-tooltip>
+                </md-button>
+            </md-dialog>
+            `,
+            controller: function DialogController($scope, $mdDialog, ticket, location, toggleFavorite) {                
+                $scope.ticket = ticket;
+                $scope.location = location;
+                $scope.toggleFavorite = toggleFavorite
+                $scope.closeDialog = function() {
+                    $mdDialog.hide();
+                };
+            }
+          });
+    }
   }
 })();
-
-
-
-window.data = [
-    {
-        title: "Section 100",
-        description: "",
-        price: 300,
-        row: 'row 2',
-        amount: 3,
-        view: { message: 'Unrestricted view', status: 0 },
-        area: "Lower Tier"
-    },
-    {
-        title: "Section 200",
-        description: "",
-        price: 110,
-        row: 'row 2',
-        amount: 1,
-        view: { message: 'Unrestricted view', status: 0 },
-        area: "Middle Tier"
-    },
-    {
-        title: "Section 105",
-        description: "",
-        price: 250,
-        row: 'row 2',
-        amount: 2,
-        view: { message: 'Unrestricted view', status: 0 },
-        area: "Lower Tier"
-    },
-    {
-        title: "Section 210",
-        description: "",
-        price: 100,
-        row: 'row 2',
-        amount: 2,
-        view: { message: 'Unrestricted view', status: 0 },
-        area: "Middle Tier"
-    },
-    {
-        title: "Section 335",
-        description: "",
-        price: 55,
-        row: 'row 2',
-        amount: 4,
-        view: { message: 'Unrestricted view', status: 0 },
-        area: "Middle Tier"
-    },
-    {
-        title: "Section 300",
-        description: "",
-        price: 50,
-        row: 'row 2',
-        amount: 1,
-        view: { message: 'Unrestricted view', status: 0 },
-        area: "Middle Tier"
-    },
-    {
-        title: "Section 400",
-        description: "",
-        price: 10,
-        row: 'row 2',
-        amount: 2,
-        view: { message: 'Unrestricted view', status: 0 },
-        area: "Upper Tier"
-    },
-    {
-        title: "Section 10",
-        description: "",
-        price: 1000,
-        row: 'row 2',
-        seat: 'seat 5',
-        amount: 3,
-        view: { message: 'Unrestricted view', status: 0 },
-        area: "Boxes"
-    }
-];
---
+++ data.js
@@ -0,0 +1,98 @@
+window.data = [
+    {
+        title: "section 110",
+        description: "Amazing seats in the lower tier in a good price",
+        price: 300,
+        section: '110',
+        row: 2,
+        seat: [1,2,3],
+        amount: 3,
+        view: { message: 'Unrestricted view', status: 0, subMsg: `You'll have a clear view of the game` },
+        score: 9.1,
+        area: "Lower Tier"
+    },
+    {
+        title: "section 216",
+        description: "Good ticket in the middle tier",
+        price: 110,
+        section: '216',
+        row: 2,
+        seat: 5,
+        amount: 1,
+        view: { message: 'Good finding', status: 0, subMsg: `Good price for ticket` },
+        score: 8.1,
+        area: "Middle Tier"
+    },
+    {
+        title: "section 101",
+        description: "Amazing tickets in the lower tier with great view",
+        price: 350,
+        section: '101',
+        row: 2,
+        seat: [5, 6],
+        amount: 2,
+        view: { message: 'Unrestricted view', status: 0, subMsg: `You'll have a clear view of the game` },
+        score: 9.0,
+        area: "Lower Tier"
+    },
+    {
+        title: "section 210",
+        description: "Middle tier seats in a good price",
+        price: 100,
+        section: '210',
+        row: 2,
+        seat: [10,11],
+        amount: 2,
+        view: { message: 'Good finding', status: 0, subMsg: `Good price for ticket` },
+        score: 8.8,
+        area: "Middle Tier"
+    },
+    {
+        title: "section 325",
+        description: "Seats at the upper tier behind the basket",
+        price: 25,
+        section: '325',
+        row: 2,
+        seat: [15,16,17,18],
+        amount: 4,
+        view: { message: 'Unrestricted view', status: 0, subMsg: `You'll have a clear view of the game` },
+        score: 9.8,
+        area: "Upper Tier"
+    },
+    {
+        title: "section 318",
+        description: "Seats at the upper tier, nice view",
+        price: 50,
+        section: '318',
+        row: 2,
+        seat: [11],
+        amount: 1,
+        view: { message: 'Unrestricted view', status: 0, subMsg: `You'll have a clear view of the game` },
+        score: 9.3,
+        area: "Upper Tier"
+    },
+    {
+        title: "section 302",
+        description: "Seats at the upper tier, good view",
+        price: 60,
+        section: '302',
+        row: 2,
+        seat: [13,14],
+        amount: 2,
+        view: { message: 'Unrestricted view', status: 0, subMsg: `You'll have a clear view of the game` },
+        score: 7.5,
+        area: "Upper Tier"
+    },
+    {
+        title: "section 10",
+        description: "Box seats, includes TV and free drinks",
+        price: 1000,
+        section: '10',
+        row: 2,
+        seat: [3,4,5],
+        amount: 3,
+        view: { message: 'Unrestricted view', status: 0, subMsg: `You'll have a clear view of the game` },
+        score: 8.5,
+        area: "Boxes"
+    }
+];
