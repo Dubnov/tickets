add13857b01f8827bf05af16c9ba243fcd5397ee
Dor Dubnov <ddubnov@gmail.com>
2019-01-05 17:51:22 +0200

    f

--- app.js
+++ app.js
@@ -1,189 +1,251 @@
-(function() {
-  "use strict";
+(function () {
+    "use strict";
 
-  angular
-    .module("app", ["ngMaterial", "ngMessages", "rzModule"])
-    .controller("main", controller);
+    angular
+        .module('app', ['ngMaterial', 'ngMessages', 'rzModule', 'infinite-scroll'])
+        .controller('main', controller);
 
-  controller.$inject = ['$mdDialog', '$mdToast'];
+    controller.$inject = ['$mdDialog', '$mdToast', '$window'];
 
-  function controller($mdDialog, $mdToast) {
-    const self = this;
-    self.eventLocation = 'Staples Center, Los Angeles, California, USA';
-    self.ticketsAmount = '';
-    self.sortOption = "Recommended";
-    
-    self.sortOptions = [
-      "Recommended",
-      "Cheapest",
-      "Best seats",
-      "Highest price"
-    ];
-    self.date = new Date();
-    self.areaFilter = {};
+    function controller($mdDialog, $mdToast, $window) {
+        const self = this;
+        self.eventLocation = 'Staples Center, Los Angeles, California, USA';
+        self.ticketsAmount = '';
+        self.sortOption = "Recommended";
+        self.pageSize = 8;
+        self.showFav = undefined;
+        self.ticketsLimit = self.pageSize;
 
-    self.$onInit = () => {
-        self.initTickets();
-        self.initRangePrices();
-        self.initAmountFilter();
-        self.initAreasFilter(); 
-        self.sortBy();
-    };
+        self.sortOptions = [
+            "Recommended",
+            "Cheapest",
+            "Best Seats",
+            "Highest Price"
+        ];
+        self.date = new Date();
+        self.areaFilter = {};
 
-    self.initAmountFilter = () => {
-        self.ticketsAmountOptions = [];
+        self.$onInit = () => {
+            self.initTickets();
+            self.initRangePrices();
+            self.initAmountFilter();
+            self.initAreasFilter();
+            self.initRowsRange();
+            self.initSeatsRange();
+            self.initScoreRange();
+            self.sortBy();
+        };
 
-        self.tickets.map(x => x.amount).forEach(x => {
-            if (!self.ticketsAmountOptions.some(a => a === x)) {
-                self.ticketsAmountOptions.push(x);
+        self.hideAdvancedSearch = () => {
+            self.isAdvancedSearch = false;
+            self.initRowsRange();
+            self.initSeatsRange();
+            scoreSlider
+        }
+
+        self.showFavorites = () => {
+            self.showFav = !self.showFav;
+            if (!self.showFav) {
+                self.showFav = undefined;
             }
-        })
+        };
 
-        self.ticketsAmountOptions.sort();
-    }
+        self.loadTickets = () => {
+            self.ticketsLimit = self.ticketsLimit + self.pageSize;
+            // self.ticketsDisplayed = self.tickets.slice(0, self.ticketsDisplayed.length + self.pageSize)
+        };
 
-    self.initAreasFilter = () => {
-      self.areas = [];
+        self.initAmountFilter = () => {
+            self.ticketsAmountOptions = [];
 
-      self.tickets
-        .map(x => x.area)
-        .forEach(x => {
-          if (!self.areas.some(a => a === x)) {
-            self.areas.push(x);
-            self.areaFilter[x] = true;
-          }
-        });
-    };
+            self.tickets.map(x => x.amount).forEach(x => {
+                if (!self.ticketsAmountOptions.some(a => a === x)) {
+                    self.ticketsAmountOptions.push(x);
+                }
+            })
 
-    self.selectAmount = (amount) => {
-        self.ticketsAmount = amount;
-    }
-
-    self.initRangePrices = () => {
-      const prices = self.tickets.map(x => x.price);
-      self.priceSlider = {
-        minValue: Math.min(...prices),
-        maxValue: Math.max(...prices),
-        options: {
-          floor: Math.min(...prices),
-          ceil: Math.max(...prices)
+            self.ticketsAmountOptions.sort();
         }
-      };
-    };
 
-    self.buyTicket = ($event) => {
-        $event.stopPropagation();
-        $mdToast.show(
-            $mdToast.simple()
-            .textContent('Thank you for buying!')
-            .position('')
-            .hideDelay(3000))
-    }
+        self.initAreasFilter = () => {
+            self.areas = [];
 
+            self.tickets
+                .map(x => x.area)
+                .forEach(x => {
+                    if (!self.areas.some(a => a === x)) {
+                        self.areas.push(x);
+                        self.areaFilter[x] = true;
+                    }
+                });
+        };
 
-    self.toggleFavorite = ($event, ticket) => {
-        $event.stopPropagation();
-        ticket.favorite = !ticket.favorite
-
-        if (ticket.favorite) {
-            $mdToast.show(
-                $mdToast.simple()
-                .textContent('The ticket was successfully added to your favorites!')
-                .position('')
-                .hideDelay(3000))
-        } else {
-            $mdToast.show(
-                $mdToast.simple()
-                .textContent('The ticket was removed from your favorites')
-                .position('')
-                .hideDelay(3000))
+        self.selectAmount = (amount) => {
+            self.ticketsAmount = amount;
         }
-    }
-    self.areaFilterFunc = tick => {
-      return self.areaFilter[tick.area];
-    };
 
-    self.sortBy = option => {
-      self.sortOption = option || "Recommended";
-      self.sortTickets(self.sortOption);
-    };
-
-    self.priceRangeFilter = tick => {
-      return (
-        tick.price >= self.priceSlider.minValue &&
-        tick.price <= self.priceSlider.maxValue
-      );
-    };
-
-    self.sortTickets = option => {
-      const tempTickets = angular.copy(self.tickets);
-      switch (option) {
-        case "Cheapest": {
-          self.tickets = tempTickets.sort((a, b) => a.price - b.price);
-          break;
-        }
-        case "Highest price": {
-          self.tickets = tempTickets.sort((a, b) => b.price - a.price);
-          break;
-        }
-        case "Best Seats": {
-          self.tickets = tempTickets.sort((a, b) => b.price - a.price);
-          break;
-        }
-        case "Recommended": {
-          self.tickets = tempTickets.sort((a, b) => b.score - a.score);
-          break;
-        }
-        default: {
-          self.tickets = tempTickets.sort((a, b) => b.price - a.price);
-          break;
-        }
-      }
-    };
-
-    self.greaterThan = function(prop, val){
-        return function(item){
-          return val ? item[prop] > val : true;
-        }
-    }
-
-    self.openImage = ($event, imageUrl) => {
-      
-        $mdDialog.show({
-            clickOutsideToClose: true,
-            targetEvent: $event,
-            bindToController: true,
-            controllerAs: 'dialogCtrl',
-            fullscreen: true,
-            template: `<img src="${imageUrl}">`,
-            controller: function DialogController($scope, $mdDialog) {
-            $scope.closeDialog = function() {
-                $mdDialog.hide();
+        self.initRangePrices = () => {
+            const prices = self.tickets.map(x => x.price);
+            self.priceSlider = {
+                minValue: Math.min(...prices),
+                maxValue: Math.max(...prices),
+                options: {
+                    floor: Math.min(...prices),
+                    ceil: Math.max(...prices)
+                }
             };
+        };
+
+        self.initRowsRange = () => {
+            self.rowSlider = {
+                minValue: 1,
+                maxValue: 30,
+                options: {
+                    floor: 1,
+                    ceil: 30
+                }
             }
-        });
-        $event.stopPropagation();
-    };
+        }
 
-    self.initTickets = () => {
-        self.tickets = window.data;
-        self.ticketsDisplayed = angular.copy(self.tickets);
-    };
+        self.initSeatsRange = () => {
+            self.seatSlider = {
+                minValue: 1,
+                maxValue: 30,
+                options: {
+                    floor: 1,
+                    ceil: 30
+                }
+            }
+        }
 
-    self.openTicket = (ticket, $event) => {
-        $event.stopPropagation();
-        $mdDialog.show({
-            clickOutsideToClose: true,
-            targetEvent: $event,
-            locals: { ticket, location: self.eventLocation, toggleFavorite: self.toggleFavorite },
-            controllerAs: 'dialogCtrl',
-            fullscreen: true,
-            template: `
+        self.initScoreRange = () => {
+            self.scoreSlider = {
+                minValue: 1,
+                maxValue: 10,
+                options: {
+                    floor: 1,
+                    ceil: 10
+                }
+            }
+        }
+
+        self.advancedFilter = (tick) => {
+            return (tick.row >= self.rowSlider.minValue && tick.row <= self.rowSlider.maxValue ) &&
+                    (tick.seat[0] >= self.seatSlider.minValue && tick.seat[tick.seat.length-1] <= self.seatSlider.maxValue) &&
+                    (tick.score >= self.scoreSlider.minValue && tick.score <= self.scoreSlider.maxValue);
+        }
+
+        self.buyTicket = ($event) => {
+            $event.stopPropagation();
+            $mdToast.show(
+                $mdToast.simple()
+                    .textContent('Thank you for buying!')
+                    .position('')
+                    .hideDelay(3000));
+        }
+
+
+        self.toggleFavorite = ($event, ticket) => {
+            $event.stopPropagation();
+            ticket.favorite = !ticket.favorite
+
+            if (ticket.favorite) {
+                $mdToast.show(
+                    $mdToast.simple()
+                        .textContent('The ticket was successfully added to your favorites!')
+                        .position('')
+                        .hideDelay(3000));
+            } else {
+                $mdToast.show(
+                    $mdToast.simple()
+                        .textContent('The ticket was removed from your favorites')
+                        .position('')
+                        .hideDelay(3000));
+            }
+        }
+        self.areaFilterFunc = tick => {
+            return self.areaFilter[tick.area];
+        };
+
+        self.sortBy = option => {
+            self.sortOption = option || "Recommended";
+            self.sortTickets(self.sortOption);
+        };
+
+        self.priceRangeFilter = tick => {
+            return (
+                tick.price >= self.priceSlider.minValue &&
+                tick.price <= self.priceSlider.maxValue
+            );
+        };
+
+        self.sortTickets = option => {
+            const tempTickets = angular.copy(self.tickets);
+            switch (option) {
+                case "Cheapest": {
+                    self.tickets = tempTickets.sort((a, b) => a.price - b.price);
+                    break;
+                }
+                case "Highest Price": {
+                    self.tickets = tempTickets.sort((a, b) => b.price - a.price);
+                    break;
+                }
+                case "Best Seats": {
+                    self.tickets = tempTickets.sort((a, b) => b.score - a.score);
+                    break;
+                }
+                case "Recommended": {
+                    self.tickets = tempTickets.sort((a, b) => b.score - a.score);
+                    break;
+                }
+                default: {
+                    self.tickets = tempTickets.sort((a, b) => b.score - a.score);
+                    break;
+                }
+            }
+        };
+
+        self.greaterThan = function (prop, val) {
+            return function (item) {
+                return val ? item[prop] > val : true;
+            }
+        }
+
+        self.openImage = ($event, imageUrl) => {
+            $mdDialog.show({
+                clickOutsideToClose: true,
+                targetEvent: $event,
+                bindToController: true,
+                controllerAs: 'dialogCtrl',
+                fullscreen: true,
+                template: `<img src="${imageUrl}">`,
+                controller: function DialogController($scope, $mdDialog) {
+                    $scope.closeDialog = function () {
+                        $mdDialog.hide();
+                    };
+                }
+            });
+            $event.stopPropagation();
+        };
+
+        self.initTickets = () => {
+            self.tickets = window.data;
+        };
+
+        self.openTicket = (ticket, $event) => {
+            $event.stopPropagation();
+            $mdDialog.show({
+                clickOutsideToClose: true,
+                targetEvent: $event,
+                locals: { ticket, location: self.eventLocation, toggleFavorite: self.toggleFavorite, buyTicket: self.buyTicket },
+                controllerAs: 'dialogCtrl',
+                fullscreen: true,
+                template: `
             <md-dialog class="ticket-entity-modal" layout="row" layout-align="space-between">
                 <div class="ticket-main" layout="row" layout-align="start start" layout-margin>
                     <div layout="column" layout-align="start center">
-                        <img class="ticket-image" src="https://cdn.viagogo.net/img/svgpng4/77551/256963.png" alt=""
-                            ng-click="ctrl.openImage($event, 'https://cdn.viagogo.net/img/svgpng4/77551/256963.png')">
+                        <img class="ticket-image" src="assets/images/section {{ticket.section}}.png" alt=""
+                            ng-click="ctrl.openImage($event, 'assets/images/section {{ticket.section}}.png')">
                         <div layout="column" layout-align="start stretch">
                             <span>
                                 Area <strong style="color: #287ac7;">{{ticket.area}}</strong>
@@ -219,7 +281,7 @@
                     </div>
                 </div>
                 <div class="ticket-buy" layout="column" layout-align="end center">
-                    <md-button class="buy-btn md-raised md-primary">Buy</md-button>
+                    <md-button class="buy-btn md-raised md-primary" ng-click="buyTicket($event); closeDialog();">Buy</md-button>
                 </div>
                 <md-button ng-if="!ticket.favorite" class="favorite md-icon-button md-accent" aria-label="Favorite" ng-click="toggleFavorite($event, ticket)">
                     <img  src="assets/icons/favorite-border.svg" alt="favorite">
@@ -231,15 +293,17 @@
                 </md-button>
             </md-dialog>
             `,
-            controller: function DialogController($scope, $mdDialog, ticket, location, toggleFavorite) {                
-                $scope.ticket = ticket;
-                $scope.location = location;
-                $scope.toggleFavorite = toggleFavorite
-                $scope.closeDialog = function() {
-                    $mdDialog.hide();
-                };
-            }
-          });
+                controller: function DialogController($scope, $mdDialog, ticket, location, toggleFavorite, buyTicket) {
+                    $scope.ticket = ticket;
+                    $scope.location = location;
+                    $scope.toggleFavorite = toggleFavorite
+                    $scope.buyTicket = buyTicket;
+
+                    $scope.closeDialog = function () {
+                        $mdDialog.hide();
+                    };
+                }
+            });
+        }
     }
-  }
 })();
